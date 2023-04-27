a0de6b232ac9ec057d4cf161b5fa3c84931a965c
Timo <tpripon99@gmail.com>
2023-04-27 18:25:49 +0300

    adding a FOR

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function setup() {
     createCanvas(500, 500);
 
+    for (let i = 0; i < 28; i++) {
+        let boxColor = color(random(256), random(256), random(256));
+        boxes[i] = new Box(i + 1, boxColor);
 }
