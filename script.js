598f562bbcb449b49aa71bc6df2983fffd0b0845
Timo <tpripon99@gmail.com>
2023-04-27 18:27:51 +0300

    adding draw function

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function setup() {
         let boxColor = color(random(256), random(256), random(256));
         boxes[i] = new Box(i + 1, boxColor);
 }
+}
+
+function draw() {
+    background(255);
+}
